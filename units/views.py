a64ab1eee6f2fd508362b50f947b7a7d9067c805
DerZander <timo.zander@outlook.com>
2020-02-02 23:58:28 +0100

    Refactor Unit View with amount and Leaders

--- units/views.py
+++ units/views.py
@@ -14,13 +14,24 @@ def index(request):
     e_user = check_einheitsfueher(request.user)
     user_permissions = {'a_user':a_user, 'e_user':e_user}
 
-    context = {'units':Unit.objects.exclude(slug="Stadt"),'user_permissions':user_permissions}
+    context = {'nav_units':Unit.objects.exclude(slug="Stadt"),'user_permissions':user_permissions}
     return render(request, template_name="units/unit_index.html",context=context)
 
 def detail(request, unit):
-   # a_user = check_admin(request.user)
-   # e_user = check_einheitsfueher(request.user)
-    #user_permissions = {'a_user':a_user, 'e_user':e_user}
+    a_user = check_admin(request.user)
+    e_user = check_einheitsfueher(request.user)
+    user_permissions = {'a_user':a_user, 'e_user':e_user}
 
-    context = {'units':Unit.objects.exclude(slug="Stadt")}#,'user_permissions':user_permissions}
-    return render(request, template_name="units/unit_index.html",context=context)
+    nav_units = Unit.objects.exclude(slug="Stadt")
+    unit = Unit.objects.get(slug=unit)
+    leaderships = Profile.objects.all().filter(unit=unit.id,role=ROLE_EINHEITSFUEHRUNG)
+    young_leaderships = Profile.objects.all().filter(unit=unit.id,role=ROLE_JUGENDWART)
+
+
+    amount_active = Profile.objects.all().filter(unit=unit.id,group=GROUP_ACTIVE).count()
+    
+    amount_young = Profile.objects.all().filter(unit=unit.id,group=GROUP_JUGEND).count()
+    print("Anzahl der Mitglieder=",amount_young)
+
+    context = {'nav_units':nav_units,'unit':unit,'young_leaderships':young_leaderships,'leaderships':leaderships,'user_permissions':user_permissions}
+    return render(request, template_name="units/unit_detail.html",context=context)
