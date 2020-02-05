860aeff40c3ae195574f3eadd7a2ac636f753518
DerZander <timo.zander@outlook.com>
2020-02-05 19:46:23 +0100

    (feat): first functions

--- documents/views.py
+++ documents/views.py
@@ -1,9 +1,48 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 from django.views import generic
 from .models import *
+from webapp.utils import *
+from django.http import Http404  
+import os
+
+from django.http import FileResponse
+from django.utils.text import slugify
+
+
 
 def index(request):
-    context = {'documents':Filedocument.objects.all()}
+    documents = Filedocument.objects.all()
+    c_admin = check_admin(request.user)
+    
+    context = {'documents':documents, 'c_admin':c_admin}
     return render(request, template_name="documents/index.html",context=context)
+
+
+def detail(request, name):
+    documents = Filedocument.objects.all()
+    docfile = Filedocument.objects.get(name=name)
+    c_admin = check_admin(request.user)
+
+    context = {'docfile':docfile,'documents':documents, 'c_admin':c_admin}
+    return render(request, template_name="documents/detail.html",context=context)
+
+def delete(request,pk):
+    try:
+        filedoc = Filedocument.objects.get(pk=pk)
+        if filedoc.owner.id == request.user.id or check_admin(request.user) == True:
+            filedoc.delete()
+    finally:
+        return redirect(reverse('documents:documents_index'))
+
+
+def document_download(request, id):
+    item = get_object_or_404(Filedocument, pk=id)
+    file_name, file_extension = 	 os.path.splitext(item.document.file.name)
+    file_extension = file_extension[1:] # removes dot
+    response = FileResponse(item.document.file, 
+        content_type = "file/%s" % file_extension)
+    response["Content-Disposition"] = "attachment;"\
+        "filename=%s.%s" %(slugify(item.name)[:100], file_extension)
+    return response
