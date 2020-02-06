f7de5d4c003632b68e5a28ea21b033cdb317cd09
DerZander <timo.zander@outlook.com>
2020-02-06 23:34:44 +0100

    (feat): create the upload

--- documents/models.py
+++ documents/models.py
@@ -3,9 +3,9 @@ from django.db import models
 class Filedocument(models.Model):
     name = models.CharField(max_length=256, unique=True)
     description = models.TextField(null=True, blank=True)
-    document = models.FileField(upload_to="files", max_length=254)
+    document = models.FileField(upload_to="files/")
     owner = models.ForeignKey(to= 'users.CustomUser',on_delete=models.DO_NOTHING, null=True, blank=True)
-    published = models.DateField(null=True, blank=True, auto_now=True, editable=False)
+    published = models.DateField(null=True, blank=True, auto_now_add=True, editable=False)
     updated = models.DateField(null=True, blank=True)
     
 
